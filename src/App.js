8bfb641cb75b4488795bae0c154ca3bc77fbf8d1
Katie Robinson <katierobinson110589@gmail.com>
2019-02-28 14:34:51 +0000

    share test with team

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <iframe
+          key="chatbot"
+          allow="microphone;"
+          width="350"
+          height="430"
+          src="https://console.dialogflow.com/api-client/demo/embedded/e2f215ec-9007-4cd1-a248-34fd1ddf18bc"
+        />
       </div>
     );
   }
